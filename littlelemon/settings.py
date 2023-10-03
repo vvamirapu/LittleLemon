b167a031ba1097813426fed86b6f66629fa53767
Vasantha Amirapu <vasantha@MacAirM2.lan>
2023-10-02 17:10:09 -0700

    adding updates to database connections

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -79,6 +79,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mydb': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'New_LittleLemon',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
