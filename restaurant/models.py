c26e0e88ad513c41244a19dd7369ff372cf8ed83
Vasantha Amirapu <vasantha@MacAirM2.lan>
2023-10-02 17:36:40 -0700

    models added for restaurant and migrations

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=0)
+    booking_date = models.DateTimeField()
+
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField(default=0)
