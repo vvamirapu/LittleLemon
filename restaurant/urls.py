f3330c27b30cd1cb0966e4e2c9dbe55a7aeb9dfd
Vasantha Amirapu <vasantha@MacAirM2.lan>
2023-10-02 17:56:25 -0700

    adding views and urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .import views
+from django.contrib import admin
+
+from . import views
+
 
 urlpatterns = [
-    # ...,
-    path('', views.index, name='index')
-    
+    path('', views.index, name='index'),
+    path('sayHello', views.sayHello, name='sayHello')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
 def index(request):
-    return render(request, 'index.html',{})
+    return render(request, 'index.html', {})
 
 # Create your views here.
+
+
+def sayHello(request):
+    return HttpResponse('Hello World')
