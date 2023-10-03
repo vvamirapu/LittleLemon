6dfcdb2595e70a5318be055182b401557e769906
Vasantha Amirapu <vasantha@MacAirM2.lan>
2023-10-02 17:41:55 -0700

    updated database settings

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'mydb': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'New_LittleLemon',
         'USER': 'root',
