6bce5b05a649c556231beb7b4539fe03075b44f6
Vasantha Amirapu <v.amirapu@icloud.com>
2023-10-06 10:12:43 -0700

    adding little lemon api for menu items

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 
 # Create your models here.
-class MenuItem(models.Model):
+
+class BaseModel(models.Model):
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class MenuItem(BaseModel):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from django.contrib import admin
 
 from . import views
-
 
 urlpatterns = [
     # path('', views.index, name='index'),
