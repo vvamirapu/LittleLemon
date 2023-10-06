ea0673ae011353652e6d7d0c14b645b3cbbbdb2d
Vasantha Amirapu <v.amirapu@icloud.com>
2023-10-06 13:38:48 -0700

    adding auth to restaurant calls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
@@ -7,4 +8,5 @@ urlpatterns = [
     # path('sayHello', views.sayHello, name='sayHello'),
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-auth-token/', obtain_auth_token)
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import permission_classes
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
@@ -18,11 +20,13 @@ def sayHello(request):
     return HttpResponse('Hello World')
 
 
+@permission_classes([IsAuthenticated])
 class MenuItemsView(ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 
+@permission_classes([IsAuthenticated])
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
@@ -31,3 +35,4 @@ class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
