fe5dd0a34db3257c9cf7212a79b63969033f7307
Vasantha Amirapu <v.amirapu@icloud.com>
2023-10-06 12:00:56 -0700

    adding authentication to api

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,4 +1,8 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import MenuItem
 from .serializers import MenuItemSerializer
@@ -6,6 +10,8 @@ from .serializers import MenuItemSerializer
 
 # Create your views here.
 class MenuItemsView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
@@ -13,3 +19,10 @@ class MenuItemsView(ListCreateAPIView):
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+
+@api_view()
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message": "This view is protected"})
