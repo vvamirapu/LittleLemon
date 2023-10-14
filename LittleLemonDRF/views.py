c4c630c9f7647f1d49916b5f53c126557c4d6788
Vasantha Amirapu <v.amirapu@icloud.com>
2023-10-13 21:57:00 -0700

    littlelemon api using DRF

---
+++ LittleLemonDRF/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+
+
+# Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
